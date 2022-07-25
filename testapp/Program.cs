576a1f1477327c8233e79cabd3d415dbde46cd88
Roman Zhykhor <romanzhykhor@gmail.com>
2022-07-25 16:04:22 +0300

    Добавьте файлы проекта.

---
+++ testapp/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace MyNamespace;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+        Console.WriteLine(Order(input));
+    }
+
+    public static string Order(string order)
+    {
+        return string.IsNullOrEmpty(order) ? string.Empty : string.Join(" ", order
+            .Split(" ")
+            .Select(x => x.Trim())
+            .Where(n => n.Length > 0)
+            .ToDictionary(x => x, a => a.Sum(c => c - '0'))
+            .OrderBy(x => x.Value)
+            .ThenBy(x => x.Key)
+            .Select(x => x.Key));
+    }
+
+
+}
